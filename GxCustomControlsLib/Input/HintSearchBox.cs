2b57cddde34533422dd434779eddb8a936865417
Carlos Millán <charlyomo@gmail.com>
2016-01-08 14:39:27 -0600

    Se agregó evento de búsqueda para control HintSearchBox

--- GxCustomControlsLib/Input/HintSearchBox.cs
+++ GxCustomControlsLib/Input/HintSearchBox.cs
@@ -19,11 +19,18 @@ namespace Gestionix.POS
     public class HintSearchBox : HintTextBox
     {
         public static readonly DependencyProperty IsSearchingProperty = DependencyProperty.Register("IsSearching", typeof(bool), typeof(HintSearchBox), new PropertyMetadata(false));
+        public static readonly RoutedEvent SearchEvent = EventManager.RegisterRoutedEvent("Search", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(HintSearchBox));
         
         public bool IsSearching
         {
             get { return (bool)GetValue(IsSearchingProperty); }
             set { SetValue(IsSearchingProperty, value); }
+        }
+
+        public event RoutedEventHandler Search
+        {
+            add { AddHandler(SearchEvent, value); }
+            remove { RemoveHandler(SearchEvent, value); }
         }
 
         static HintSearchBox()
@@ -35,7 +42,15 @@ namespace Gestionix.POS
         {
             base.OnApplyTemplate();
             Button CloseActionButton = this.GetTemplateChild("PART_SecundaryIcon") as Button;
-            CloseActionButton.Click += CloseActionButton_Click;           
+            Button SearchActionButton = this.GetTemplateChild("PART_PrimaryIcon") as Button;
+            CloseActionButton.Click += CloseActionButton_Click;
+            SearchActionButton.Click += SearchActionButton_Click;
+        }
+
+        private void SearchActionButton_Click(object sender, RoutedEventArgs e)
+        {
+            IsSearching = true;
+            RaiseEvent(new RoutedEventArgs(HintSearchBox.SearchEvent));
         }
              
         private void CloseActionButton_Click(object sender, RoutedEventArgs e)
@@ -48,8 +63,11 @@ namespace Gestionix.POS
         {
             base.OnPreviewKeyDown(e);
 
-            if (e.Key == Key.Enter)            
-                IsSearching = true;            
+            if (e.Key == Key.Enter)
+            {
+                IsSearching = true;
+                RaiseEvent(new RoutedEventArgs(HintSearchBox.SearchEvent));
+            }
         }
     }    
 }
--- Test/MainWindow.xaml.cs
+++ Test/MainWindow.xaml.cs
@@ -31,17 +31,29 @@ namespace Test
             bwtest = new BackgroundWorker();
             bwtest.DoWork += bwtest_DoWork;
             bwtest.RunWorkerCompleted += bwtest_RunWorkerCompleted;
+            bwtest.WorkerReportsProgress = true;
+            bwtest.ProgressChanged += bwtest_ProgressChanged;
+        }
+
+        void bwtest_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            TxtSearchingResult.Text = "buscando..." + e.ProgressPercentage.ToString();
         }
 
         void bwtest_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
             HintSearchBox T = (HintSearchBox)e.Result;
             T.IsSearching = false;
+            TxtSearchingResult.Text = "ya buscó algo";
         }
 
         void bwtest_DoWork(object sender, DoWorkEventArgs e)
         {
-            Thread.Sleep(1000);
+            for (int seconds = 0; seconds < 3; seconds++)
+            {
+                bwtest.ReportProgress(seconds + 1);
+                Thread.Sleep(1000);
+            }
             e.Result = e.Argument;
         }
 
@@ -54,8 +66,10 @@ namespace Test
         {
             if (e.Key == Key.Enter)
             {
-                HintSearchBox t = (HintSearchBox)sender;                
-                bwtest.RunWorkerAsync(t);
+                HintSearchBox t = (HintSearchBox)sender;
+
+                if(!bwtest.IsBusy)
+                    bwtest.RunWorkerAsync(t);
             }
         }
 
@@ -67,6 +81,11 @@ namespace Test
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             RingTest.IsActivated = true;
+        }
+
+        private void TxtSeatch_Search(object sender, RoutedEventArgs e)
+        {
+            bwtest.RunWorkerAsync(sender as HintSearchBox);
         }
     }
 }
