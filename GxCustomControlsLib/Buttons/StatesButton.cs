38a229f0468dd256d88d12b1dd87e00c4781e1f5
CarlosMillan <charlyomo@gmail.com>
2016-03-29 12:03:51 -0600

    Los botones ya cuenta con un hover que es de acuerdo al background, se incrementa la tonalidad. El cuadro Modal muestra animación de inicion y de fin

--- GxCustomControlsLib/Buttons/StatesButton.cs
+++ GxCustomControlsLib/Buttons/StatesButton.cs
@@ -125,7 +125,10 @@ namespace Gestionix.POS
 
         public override void OnApplyTemplate()
         {
-            base.OnApplyTemplate();            
+            base.OnApplyTemplate();
+
+            if(HoverBrush == null) SetValue(HoverBrushProperty, IncreasedColor((SolidColorBrush)this.Background));
+
             Ring.Width = this.Height / 2;
             Ring.Height = this.Height / 2;
         }
@@ -137,6 +140,25 @@ namespace Gestionix.POS
                 this.IsBusy = true;
                 this.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
             }
+        }
+
+        private Brush IncreasedColor(SolidColorBrush basecolor)
+        {
+            float IncrementPercentage = .20f;
+            int[] RGB = new int[3];
+            RGB[0] = basecolor.Color.R;
+            RGB[1] = basecolor.Color.G;
+            RGB[2] = basecolor.Color.B;
+
+            RGB[0] += (int)(RGB[0] * IncrementPercentage);
+            RGB[1] += (int)(RGB[1] * IncrementPercentage);
+            RGB[2] += (int)(RGB[2] * IncrementPercentage);
+
+            RGB[0] = RGB[0] > 255 ? 255 : RGB[0];
+            RGB[1] = RGB[1] > 255 ? 255 : RGB[1];
+            RGB[2] = RGB[2] > 255 ? 255 : RGB[2];
+            
+            return new SolidColorBrush(Color.FromRgb((byte)RGB[0], (byte)RGB[1], (byte)RGB[2]));
         }
     }
 }
--- GxCustomControlsLib/UserControls/ModalPopup.xaml.cs
+++ GxCustomControlsLib/UserControls/ModalPopup.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -49,12 +50,18 @@ namespace Gestionix.POS
                 if (M.IsActive)
                 {
                     M.Visibility = Visibility.Visible;
+                    M.FadeInAnimation(.3f);
                     M.Target.IsEnabled = false;
                 }
                 else
                 {
-                    M.Visibility = Visibility.Collapsed;
-                    M.Target.IsEnabled = true;
+                    M.FadeOutAnimation(.3f, 
+                                       new EventHandler((s, er) => 
+                                       {
+                                           M.Visibility = Visibility.Collapsed;
+                                           M.Target.IsEnabled = true;
+                                       }));
+
                 }
             }
             else
@@ -66,6 +73,31 @@ namespace Gestionix.POS
         //    ModalPopup M = (o as ModalPopup);
 
         //}
+        #endregion
+
+        #region Methods
+        private void FadeInAnimation(float secondsduration, EventHandler oncompleteanimation = null)
+        {
+            FadeAnimation(0, .7f, secondsduration, oncompleteanimation);
+        }
+
+        private void FadeOutAnimation(float secondsduration, EventHandler oncompleteanimation = null)
+        {
+            FadeAnimation(.7f, 0, secondsduration, oncompleteanimation);
+        }
+
+        private void FadeAnimation(float from, float to, float secondsduration, EventHandler oncompleteanimation = null)
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = from;
+            da.To = to;
+            da.Duration = new Duration(TimeSpan.FromSeconds(secondsduration));
+
+            if (oncompleteanimation != null && oncompleteanimation is EventHandler)
+                da.Completed += oncompleteanimation;
+
+            this.BeginAnimation(OpacityProperty, da);
+        }
         #endregion
 
         public ModalPopup()
