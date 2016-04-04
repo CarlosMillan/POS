9962462d29e90d1624ac4dc6c8709b417a35b6ac
CarlosMillan <charlyomo@gmail.com>
2016-04-04 11:26:05 -0500

    Anillo giratorio que miestra la sincronización, echo con blend, pero aún sin la animación, pre-cargado al inicial control

---
+++ GxCustomControlsLib/Animated/SyncRing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Gestionix.POS
+{
+    public class SyncRing : Control
+    {
+        static SyncRing()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SyncRing), new FrameworkPropertyMetadata(typeof(SyncRing)));
+        }
+    }
+}
